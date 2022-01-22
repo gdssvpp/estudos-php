e81656ae008c2682a29c06d873af2c5182894d40
gdssvpp <pablonogueira16@hotmail.com>
2022-01-21 23:57:49 -0300

    nada de mais

--- Exercício 1/Exercício 1 Original/Ex1.php
+++ Exercício 1/Exercício 1 Original/Ex1.php
@@ -1,12 +1,14 @@
 <?php
 
-$val = -69;
+$val = 1976;
 $resultado = "";
 
 if($val > 0){
   $resultado = "Valor Positivo";
+
 }elseif($val < 0){
   $resultado = "Valor Negativo";
+
 }else{
   $resultado = "Igual a Zero";
 }
